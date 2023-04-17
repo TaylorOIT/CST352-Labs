8978a07476172a095a22bf8032df542b3f5059ea
TaylorOIT <taylor.boyd@oit.edu>
2023-04-16 17:40:26 -0700

    tested our SafeRing class for what we implemented.

--- CST352_Assign1/Assign1_Threads/Assign1_Threads/Program.cs
+++ CST352_Assign1/Assign1_Threads/Assign1_Threads/Program.cs
@@ -10,6 +10,18 @@ namespace Assign1_Threads
     {
         static void Main(string[] args)
         {
+            const int RING_CAPACITY = 5;
+
+            SafeRing ring = new SafeRing(RING_CAPACITY);
+
+            ring.Insert(0);
+            ring.Insert(1);
+            ring.Insert(2);
+
+            Console.WriteLine("Size = " + ring.Count().ToString());
+            Console.WriteLine(".Remove() = " + ring.Remove().ToString());
+            Console.WriteLine(".Remove() = " + ring.Remove().ToString());
+            Console.WriteLine(".Remove() = " + ring.Remove().ToString());
         }
     }
 }
