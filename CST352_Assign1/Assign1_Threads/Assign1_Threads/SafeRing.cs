4848022ef6b9e16b1fe59b5ad83a815c3b2ff766
TaylorOIT <taylor.boyd@oit.edu>
2023-04-16 17:12:03 -0700

    stubbing out the ideas of what out Insert method will do (all none working code at the moment.

--- CST352_Assign1/Assign1_Threads/Assign1_Threads/SafeRing.cs
+++ CST352_Assign1/Assign1_Threads/Assign1_Threads/SafeRing.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Assign1_Threads
@@ -18,6 +19,27 @@ namespace Assign1_Threads
 
         public void Insert(int i)
         {
+            // wait until it's safe and until there is capacity to insert
+            wait(mutex,hasCapacity);
+
+            // iunsert the item i
+            buffer[tail] = i;
+
+            // increment the tail
+            tail = (tail + 1) % capacity;
+
+            // increment the size
+            size++;
+
+            // notify any objects waiting to remove that there is an item in the queue
+            hasItems.set();
+
+            // cause any objects wanting to insert to sleep if there is no longer capacity
+            if (size == capacity)
+                hasCapacity.reset();
+
+            // release the mutex
+            mutex.unlock();
 
         }
 
