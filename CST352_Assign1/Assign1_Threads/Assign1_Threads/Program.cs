a4b4674aff2ecef480fa0bf893cc53b7f43f8321
TaylorOIT <taylor.boyd@oit.edu>
2023-04-21 22:18:04 -0700

    created some test code in the future to test the Producer class.

--- CST352_Assign1/Assign1_Threads/Assign1_Threads/Program.cs
+++ CST352_Assign1/Assign1_Threads/Assign1_Threads/Program.cs
@@ -11,10 +11,32 @@ namespace Assign1_Threads
         static void Main(string[] args)
         {
             const int RING_CAPACITY = 5;
+            const int NUM_PRODUCERS = 2;
+            const int NUM_ITEMS_TO_PRODUCE = 10;
+            const int NUM_CONSUMERS = 2;
+
+            Random rand = new Random();
 
             SafeRing ring = new SafeRing(RING_CAPACITY);
 
+            // create and start producers
+            List<Producer> producers = new List<Producer>();
+            for (int i = 0; i < NUM_PRODUCERS; i++)
+            {
+                Producer p = new Producer(rand, ring, NUM_ITEMS_TO_PRODUCE);
+                producers.Add(p);
+                p.Start();
+            } 
+            
+            // create and start the consumers
 
+            // wait until all producers are complete
+            for (int i = 0; i < NUM_PRODUCERS; i++)
+            {
+                producers[i].Complete.WaitOne();
+            }
+
+            // stop the consumers
         }
     }
 }
