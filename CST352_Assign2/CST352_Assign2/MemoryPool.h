303d70c9e94f163bb9d272f051bc53c646cf6322
TaylorOIT <taylor.boyd@oit.edu>
2023-05-07 18:06:39 -0700

    added a destructor.

--- CST352_Assign2/CST352_Assign2/MemoryPool.h
+++ CST352_Assign2/CST352_Assign2/MemoryPool.h
@@ -1,5 +1,14 @@
+// MemoryPool.h
+// virtual base class for all memory pools
+
 #pragma once
 class MemoryPool
 {
+	public:
+		MemoryPool(unsigned int poolSize);
+		virtual void* Allocate(unsigned int nBytes);
+		virtual void Free(void* block);
+		virtual void DebugPrint();
+
 };
 
