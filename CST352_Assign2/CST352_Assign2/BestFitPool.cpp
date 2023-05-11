7a9f45a501b16f9263c7589522ccf767814cd86a
TaylorOIT <taylor.boyd@oit.edu>
2023-05-10 17:56:24 -0700

    Implemented the BestFitPool class and tests for it.

--- CST352_Assign2/CST352_Assign2/BestFitPool.cpp
+++ CST352_Assign2/CST352_Assign2/BestFitPool.cpp
@@ -1,1 +1,31 @@
 #include "BestFitPool.h"
+
+
+std::vector<MemoryPool::Chunk>::iterator BestFitPool::FindAvailableChunk(unsigned int nBytes)
+{
+    // Best Fit Algorithm
+    // find the smallest available chunk that is big enough to fit nBytes (at least that big)
+    // find the first chunk that is big enough and then
+    // continue through the rest of the chunks, keep track of the smallest one we find that is big enough
+
+    // find first fit
+    std::vector<MemoryPool::Chunk>::iterator bestIter;
+    for (bestIter = chunks.begin(); bestIter != chunks.end(); bestIter++) {
+        if (!bestIter->allocated && bestIter->size >= nBytes) {
+            // we found the first fit....
+            break;
+        }
+    }
+
+    // find the best fit from there
+    for (std::vector<MemoryPool::Chunk>::iterator chunkIter = bestIter; chunkIter != chunks.end(); chunkIter++) {
+        // if we find a better one, remember that
+        if (!chunkIter->allocated && chunkIter->size >= nBytes && chunkIter->size < bestIter->size) {
+            bestIter = chunkIter;
+        }
+    }
+
+    // couldn't find one
+    return bestIter;
+}
+
--- CST352_Assign2/CST352_Assign2/CST352_Assign2.cpp
+++ CST352_Assign2/CST352_Assign2/CST352_Assign2.cpp
@@ -4,16 +4,31 @@
 #include "MemoryPool.h"
 #include "StringQueue.h"
 #include "FirstFitPool.h"
+#include "BestFitPool.h"
+
+void TestFindAlgorithm(MemoryPool* pool)
+{
+    std::cout << "inital pool..." << std::endl;
+    pool->DebugPrint();
+    StringQueue queue(pool);
+
+    queue.Insert("01234567890123456789012345678901234567890123456789");
+    queue.Insert("0123456789");
+    queue.Remove();
+    queue.Insert("012345678901234567890123456789");
+
+    std::cout << "final pool..." << std::endl;
+    pool->DebugPrint();
+}
 
 int main()
 {
     try {
-        FirstFitPool pool(100);
-        std::cout << "inital pool..." << std::endl;
-        pool.DebugPrint();
-        StringQueue queue(&pool);
+        FirstFitPool firstFitPool(100);
+        BestFitPool bestFitPool(100);
         
-        
+        TestFindAlgorithm(&firstFitPool);
+        TestFindAlgorithm(&bestFitPool);
         //queue.Insert("foo");
         //queue.Insert("bar");
         //std::cout << queue.Peek() << std::endl;
@@ -21,14 +36,6 @@ int main()
         //std::cout << queue.Peek() << std::endl;
         //queue.Remove();
         //pool.DebugPrint();
-
-        queue.Insert("01234567890123456789012345678901234567890123456789");
-        queue.Insert("0123456789");
-        queue.Remove();
-        queue.Insert("012345678901234567890123456789");
-
-        std::cout << "final pool..." << std::endl;
-        pool.DebugPrint();
     }
     catch (FullException) {
         std::cout << "FullException!" << std::endl;
